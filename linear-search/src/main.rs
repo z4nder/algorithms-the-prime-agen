9e70662655b938cba8199b2f75ce6f6d5faffe30
Gustavo Araujo <gusttavo212@gmail.com>
2023-03-27 11:53:23 -0300

    Add test

--- linear-search/src/main.rs
+++ linear-search/src/main.rs
@@ -7,6 +7,7 @@ fn main() {
     println!("The search {} is finded at position {}", search, result);
 }
 
+// Big O -> O(n)
 fn linear_search(list: Vec<i32>, search: &i32) -> Option<usize>{
 
     for (i, x) in list.iter().enumerate() {
@@ -16,4 +17,26 @@ fn linear_search(list: Vec<i32>, search: &i32) -> Option<usize>{
     }
 
     None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_search() {
+        let tests = vec![
+            (vec![], 1, None),
+            (vec![1], 1, Some(0)),
+            (vec![1, 2], 1, Some(0)),
+            (vec![1, 2], 2, Some(1)),
+            (vec![1, 2, 3], 3, Some(2)),
+            (vec![-1, 0, 3, 5, 9, 12], 9, Some(4)),
+            (vec![-1, 0, 3, 5, 9, 12], 2, None),
+        ];
+
+        for (input, target, expected) in tests {
+            assert_eq!(expected, linear_search(input, &target))
+        }
+    }
 }
